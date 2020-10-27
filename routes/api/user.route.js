18d818c36181be621e708fc67689cd58c196d389
ga-ripal <54301757+ga-ripal@users.noreply.github.com>
2020-10-27 12:59:26 +0530

    Add files via upload

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -15,11 +15,11 @@ router.post(`${ROUTES.CREATE_USER.URL}`, async(req,res,next)=>{
 const {body } = req
 const email = body.email
 const isEmailExist = await User.find({email})
-if(isEmailExist){
+if(isEmailExist && Object.keys(isEmailExist).length){
     return apiHelper.failure(res,[],ERROR_LITERALS.COMMON_MESSAGE.IS_EXISTS, GLOBAL.STATUS_CODE.BAD_REQUEST)
 }
 const userInstance =  new User({...body})
-const user = userInstance.save()
+const user = await userInstance.save()
 if(user && Object.keys(user).length){
     return apiHelper.success(res,{user}, ERROR_LITERALS.CREATE_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
 }
@@ -31,13 +31,12 @@ if(user && Object.keys(user).length){
 router.put(`${ROUTES.UPDATE_USER.URL}`, async(req,res,next)=>{
     try{
 const {body } = req
-const id = req.params
-const isEmailExist = await User.find({id})
-if(!isEmailExist){
-    return apiHelper.failure(res,[],ERROR_LITERALS.COMMON_MESSAGE.NO_DATA_FOUND, GLOBAL.STATUS_CODE.BAD_REQUEST)
-}
-const user =await User.update({...body})
-if(user && Object.keys(user).length){
+const id = req.params.id
+
+const user =await User.findByIdAndUpdate(id,{...body},{
+    new:true, upsert:true
+})
+if(user){
     return apiHelper.success(res,{user}, ERROR_LITERALS.UPDATE_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
 }
     }catch(err){
@@ -47,12 +46,9 @@ if(user && Object.keys(user).length){
 
 router.delete(`${ROUTES.DELETE_USER.URL}`, async(req,res,next)=>{
     try{
-const id = req.params
-const isEmailExist = await User.find({id})
-if(!isEmailExist){
-    return apiHelper.failure(res,[],ERROR_LITERALS.COMMON_MESSAGE.NO_DATA_FOUND, GLOBAL.STATUS_CODE.BAD_REQUEST)
-}
-const user =await User.remove({id})
+const id = req.params.id
+
+const user = await User.findByIdAndDelete(id)
 if(user && Object.keys(user).length){
     return apiHelper.success(res,{user}, ERROR_LITERALS.DELETE_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
 }
@@ -63,16 +59,13 @@ if(user && Object.keys(user).length){
 
 router.get(`${ROUTES.GET_USER.URL}`, async(req,res,next)=>{
     try{
-const {body } = req
-const id = req.params
-const isEmailExist = await User.find({id})
-if(!isEmailExist){
-    return apiHelper.failure(res,[],ERROR_LITERALS.COMMON_MESSAGE.NO_DATA_FOUND, GLOBAL.STATUS_CODE.BAD_REQUEST)
+const id = req.params.id
+const isUserExist = await User.findById(id)
+if(isUserExist){
+    return apiHelper.success(res,{isUserExist}, ERROR_LITERALS.GET_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
 }
-else{
-    return apiHelper.success(res,{user}, ERROR_LITERALS.GET_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
-}
-    }catch(err){
+return apiHelper.failure(res,[],ERROR_LITERALS.COMMON_MESSAGE.NO_DATA_FOUND, GLOBAL.STATUS_CODE.BAD_REQUEST)
+ }catch(err){
         return apiHelper.failure(res, [err], ERROR_LITERAL.COMMON_MESSAGE.CATCH_ERROR, GLOBAL.STATUS_CODE.BAD_REQUEST)
     }
 })
@@ -91,16 +84,13 @@ if(search){
 if(pageNo && limit){
     pageNo = parseInt(pageNo)||0;
     limit = parseInt(limit)||3;
-    const user = await User.find().sort({updated_at:-1}).skip(page*limit).limit(limit).exec((err,docs)=>{
-        if(err){
-            return apiHelper.failure(res,[err], ERROR_LITERAL.COMMON_MESSAGE.ERR, GLOBAL.STATUS_CODE.BAD_REQUEST)
-        }
-        if(user && Object.keys(user).length){
-            return apiHelper.success(res,{user}, ERROR_LITERAL.GET_ALL_USER_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
-        }
-
-    })
-
+   
+    const user = await User.find().sort({updated_at:-1}) .limit(limit * 1)
+    .skip((pageNo - 1) * limit)
+    .exec();
+    if(user ){
+        return apiHelper.success(res,{user}, ERROR_LITERALS.GET_ALL_USERS_SUCCESS, GLOBAL.STATUS_CODE.SUCCESS)
+    }
 }
 const user =await User.find()
 if(user && Object.keys(user).length){
@@ -111,3 +101,5 @@ return apiHelper.failure(res, [err],ERROR_LITERAL.COMMON_MESSAGE.NO_DATA_FOUND, 
         return apiHelper.failure(res, [err], ERROR_LITERAL.COMMON_MESSAGE.CATCH_ERROR, GLOBAL.STATUS_CODE.BAD_REQUEST)
     }
 })
+
+module.exports = router
